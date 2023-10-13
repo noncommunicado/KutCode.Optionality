5db72d51747ec35c1adc142aae3a00574f7acb41
kutumov.n <kutumov.n@neftm.ru>
2023-10-13 17:25:52 +0300

    Optional.Value reworked

--- src/KutCode.Optional.Core/Optional.cs
+++ src/KutCode.Optional.Core/Optional.cs
@@ -14,8 +14,34 @@ public readonly struct Optional<TValue> where TValue : class
 
 	public static implicit operator TValue?(Optional<TValue> optionalValue) => optionalValue._value;
 	public static implicit operator Optional<TValue>(TValue? value) => new(value);
-	public static Optional<TValue> None => new(null);
+	
+	/// <summary>
+	/// Produce default 
+	/// </summary>
+	public static Optional<TValue> None => _none;
+	private static Optional<TValue> _none = new(null);
+	
+	/// <summary>
+	/// Is internal init-only set value of <see cref="TValue"/> type is not null
+	/// </summary>
 	public bool HasValue => _value is not null;
-	public TValue? Value => _value;
+
+	/// <summary>
+	/// Get value if it's existed.<br/>
+	/// Throws <see cref="NullReferenceException"/> if value wasn't set.<br/>
+	/// Highly recommended to use <see cref="HasValue"/> first.
+	/// </summary>
+	/// <exception cref="NullReferenceException">Throws if value wasn't set</exception>
+	public TValue Value {
+		get {
+			if (_value is null) throw new NullReferenceException("Value is null");
+			return _value;
+		}
+	}
+
+	/// <summary>
+	/// Produce <see cref="ToString"/> call result of <see cref="TValue"/> object.<br/>
+	/// </summary>
+	/// <returns>If <see cref="TValue"/> object is null - returns <b>"null"</b> as string.</returns>
 	public override string ToString() => _value?.ToString() ?? EmptyToStringInvocationResult;
 }
--- src/KutCode.Optional.Core/OptionalValue.cs
+++ src/KutCode.Optional.Core/OptionalValue.cs
@@ -17,7 +17,23 @@ public readonly struct OptionalValue<TValue> where TValue : struct
 	public static implicit operator TValue?(OptionalValue<TValue> optionalValue) => optionalValue._value;
 	public static implicit operator OptionalValue<TValue>(TValue? value) => new(value);
 	public static OptionalValue<TValue> None => new(null);
+	
+	/// <summary>
+	/// Is internal init-only set value of <see cref="TValue"/> type is not null
+	/// </summary>
 	public bool HasValue => _value is not null;
+	
+	/// <summary>
+	/// Get value if it's existed.<br/>
+	/// Throws <see cref="NullReferenceException"/> if value wasn't set.<br/>
+	/// Highly recommended to use <see cref="HasValue"/> first.
+	/// </summary>
+	/// <exception cref="NullReferenceException">Throws if value wasn't set</exception>
 	public TValue? Value => _value;
+	
+	/// <summary>
+	/// Produce <see cref="ToString"/> call result of <see cref="TValue"/> object.<br/>
+	/// </summary>
+	/// <returns>If <see cref="TValue"/> object is null - returns <b>"null"</b> as string.</returns>
 	public override string ToString() => _value?.ToString() ?? EmptyToStringInvocationResult;
 }
